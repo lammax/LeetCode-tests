b7ef499cebaec9bf1125ca6f59f3c9e4a470fd76
Maksim Lamanskii <maksimlamanskii@bluebridgetech.com>
2022-04-29 12:26:10 +0300

    Initial Commit

---
+++ Leetcode-tests/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Leetcode-tests
+//
+//  Created by Максим Ламанский on 29.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Leetcode-tests/Leetcode_testsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leetcode_testsApp.swift
+//  Leetcode-tests
+//
+//  Created by Максим Ламанский on 29.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Leetcode_testsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
